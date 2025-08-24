61e787f9809ea22529d70e0851fe3c8219977d76
Vyom Thakkar <vyom.thakkar3508@gmail.com>
2025-08-24 12:06:41 +0530

    added requirements.txt and tested the basic closed_form toy2state

--- exact-methods/tests/test_toy2state_closed_form.py
+++ exact-methods/tests/test_toy2state_closed_form.py
@@ -2,6 +2,8 @@
 import numpy as np
 from src.rlx.envs.tabular.toy2state import build, closed_form_values
 
+
+#run python3 -m pytest tests/test_toy2state_closed_form.py -v to run the test
 def test_closed_form_match():
     gamma, p, r = 0.99, 0.5, 1.0
     mdp = build(gamma=gamma, p_loop=p, r_reward=r)
